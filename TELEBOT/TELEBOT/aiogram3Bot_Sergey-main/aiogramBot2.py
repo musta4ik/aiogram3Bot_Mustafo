3b5f77194344628e6d74e0c2a0ff3905cf39af8a
MUSTA4ik <134576351+musta4ik@users.noreply.github.com>
2023-10-18 13:57:12 +0300

    добавил бан пользователя
    банить могут все подряд

---
+++ TELEBOT/TELEBOT/aiogram3Bot_Sergey-main/aiogramBot2.py
@@ -0,0 +1,37 @@
+import logging
+import asyncio
+# from datetime import datatime
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.types import FSInputFile
+
+TELEGRAM_TOKEN ="6476180980:AAHNbYz6_ALvd6FRkx3Ad3KGmsp24eotbiE"
+
+# вывод отладочных сообщений в терминал
+logging.basicConfig(level=logging.INFO)
+
+# создали обьект bot
+bot = Bot(token=TELEGRAM_TOKEN)
+
+# создаем обьект диспетчер 
+dp = Dispatcher()
+
+# обрабатываем команду старт
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    await message.answer("Рад тебя видеть!")
+
+
+
+
+
+
+
+# непрерывный режим работы бота в АССИНХРОННОМ режиме 
+async def main():
+    await dp.start_polling(bot)
+
+# основной цикл
+if __name__ == '__main__':
+    asyncio.run(main())
